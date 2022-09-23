20fc3d8fdedc20a0f45e34633c6628f2293bb9f9
Matthew_Cash <matthew@matthew-cash.com>
2022-09-22 21:31:34 -0700

    Deserialize ISO dates

--- src/tweetRecord.ts
+++ src/tweetRecord.ts
@@ -6,6 +6,7 @@ let tweets: Tweet[];
 export const loadTweets = async () => {
     const recordFile = await fs.readFile(process.env.TWEET_RECORD_PATH);
     tweets = JSON.parse(recordFile.toString());
+    tweets.forEach(tweet => tweet.date = new Date(tweet.date));
 };
 
 export const getTweets = () => tweets;
